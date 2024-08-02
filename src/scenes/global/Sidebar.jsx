a7aaf7496574aedf01c57e0bfbe249ad70588889
ABDULLAHI <Ashleyalvins010@gmail.com>
2024-08-02 17:58:17 +0100

    Updated

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -107,7 +107,7 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Isihaq Abdullahi
+                  ADMIN
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
                   Ishaq Abdullahi 
