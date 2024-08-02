b0b53c0a6e2b97da3ce802a9d933e1beb2cd32a2
ABDULLAHI <Ashleyalvins010@gmail.com>
2024-08-02 17:08:50 +0100

    updated

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -110,7 +110,7 @@ const Sidebar = () => {
                   Isihaq Abdullahi
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
-                  VP Fancy Admin
+                  Ishaq Abdullahi 
                 </Typography>
               </Box>
             </Box>
